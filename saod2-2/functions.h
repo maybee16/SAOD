e02e694a3528e9f007dc103c83da3ab26c24b639
maybee16 <maya161100@mail.ru>
2020-03-27 02:37:58 +0900

    v0.1

---
+++ saod2-2/functions.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+void rand_mass(double mass[], unsigned n);
+double min_el(double mass[], unsigned n);
+double sum_el(double mass[], unsigned n);
+void sort_mass(double mass[], unsigned n, double a, double b);
+void output(double mass[], unsigned n);
+
