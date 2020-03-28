f50eaebfe4d42caa0387e8641bd797b04c9e0c57
maybee16 <maya161100@mail.ru>
2020-03-28 23:10:02 +0900

    v0.1

---
+++ Алгоритмы поиска/search.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+void rand_mass(int* mass, unsigned n, int min, int max);
+string search(int* mass, int n, int key);
+void input(int* mass, int n);
+void output(int* mass, int n);
